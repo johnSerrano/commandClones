56169f42e5a1d4eff2cec38661f10746f20a9f10
johnSerrano <johnSerrano@github.com>
2015-04-15 18:18:49 +0000

    removed lines in mypwd which did nothing

--- pwd/mypwd.c
+++ pwd/mypwd.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]){
-  FILE *topwd;
   char wd[1024];
   getcwd(wd, sizeof(wd));
-  topwd = fopen(wd, "r");
   printf("%s\n", wd);
   return 0;
 }
